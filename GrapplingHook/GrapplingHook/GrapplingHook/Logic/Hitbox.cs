ef4e91fc277de7ab2267d7078fbd9b9aa2dd7670
Hypopyon <daniel.silvestre@hypopyon.net>
2018-04-20 11:46:51 -0500

    Renamed AABB

--- GrapplingHook/GrapplingHook/GrapplingHook/Logic/Hitbox.cs
+++ GrapplingHook/GrapplingHook/GrapplingHook/Logic/Hitbox.cs
@@ -5,16 +5,16 @@ using System.Text;
 using Microsoft.Xna.Framework;
 
 namespace GrapplingHook.Logic {
-    class AABB {
+    class Hitbox {
         public Vector2 position;
         public Vector2 bounds;
 
-        public AABB(float xpos, float ypos, float xbnd, float ybnd) {
+        public Hitbox(float xpos, float ypos, float xbnd, float ybnd) {
             position = new Vector2(xpos, ypos);
             bounds = new Vector2(xbnd, ybnd);
         }
 
-        public bool Intersects(AABB other) {
+        public bool Intersects(Hitbox other) {
             return (position.X + bounds.X > other.position.X && position.X < other.position.X + other.bounds.X)
                 && (position.Y + bounds.Y > other.position.Y && position.Y < other.position.Y + other.bounds.Y);
         }
