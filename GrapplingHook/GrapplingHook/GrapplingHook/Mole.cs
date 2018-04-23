52690c131202cd0b0fe265167a2e80436597b619
Hypopyon <daniel.silvestre@hypopyon.net>
2018-04-23 01:00:08 -0500

    Added enemies and collectibles

--- GrapplingHook/GrapplingHook/GrapplingHook/Mole.cs
+++ GrapplingHook/GrapplingHook/GrapplingHook/Mole.cs
@@ -14,7 +14,36 @@ namespace GrapplingHook {
         
 
         public void UpdateMoles() {
+            for (int i = 0; i < Moles.Count; i++) {
+                var mole = Moles[i];
 
+                var tileX = (int)((mole.position.X - mole.position.X % 16) / 16);
+                var tileY = (int)((mole.position.Y - mole.position.Y % 16) / 16);
+
+                if (mole.velocity.X < 0) {
+                    if (tileX == 0) {
+                        mole.velocity.X *= -1;
+                    } else if (tilemap[tileX, tileY] == Tile.Wall || tilemap[tileX, tileY] == Tile.NoGrapple) {
+                        mole.velocity.X *= -1;
+                    } else if (tilemap[tileX, tileY + 1] != Tile.Wall && tilemap[tileX, tileY + 1] != Tile.NoGrapple && tilemap[tileX, tileY + 1] != Tile.OneWayPlatform) {
+                        mole.velocity.X *= -1;
+                    }
+                } else if (mole.velocity.X > 0) {
+                    if (tileX == LEVEL_WIDTH - 1) {
+                        mole.velocity.X *= -1;
+                    } else if (tilemap[tileX + 1, tileY] == Tile.Wall || tilemap[tileX + 1, tileY] == Tile.NoGrapple) {
+                        mole.velocity.X *= -1;
+                    }
+                    else if (tilemap[tileX + 1, tileY + 1] != Tile.Wall && tilemap[tileX + 1, tileY + 1] != Tile.NoGrapple && tilemap[tileX + 1, tileY + 1] != Tile.OneWayPlatform) {
+                        mole.velocity.X *= -1;
+                    }
+                }
+                
+
+                mole.position += mole.velocity;
+
+
+            }
         }
 
         public void DrawMoles() {
