143700d398c9eb171ac962345a3b52f2b8a5d88c
BuizelKing <judsonwjones@gmail.com>
2018-05-21 11:55:07 -0500

    Main Menu button in options

--- GrapplingHook/GrapplingHook/GrapplingHook/Game.cs
+++ GrapplingHook/GrapplingHook/GrapplingHook/Game.cs
@@ -144,15 +144,6 @@ namespace GrapplingHook
             gamepad = GamePad.GetState(PlayerIndex.One);
             mouse = Mouse.GetState();
 
-            if (keyboard.IsKeyDown(Keys.OemPeriod) && keyboardOld.IsKeyUp(Keys.OemPeriod)) {
-                level = (level + 1) % levelNames.Length;
-                ChangeLevel(level);
-            }
-            if (keyboard.IsKeyDown(Keys.OemComma) && keyboardOld.IsKeyUp(Keys.OemComma)) {
-                level = (level - 1) % levelNames.Length;
-                ChangeLevel(level);
-            }
-
             switch (state)
             {
                 case GameState.Title:
@@ -166,6 +157,18 @@ namespace GrapplingHook
                 case GameState.Level:
                     if (keyboard.IsKeyDown(Keys.Escape))
                         state = GameState.Pause;
+
+                    if (keyboard.IsKeyDown(Keys.OemPeriod) && keyboardOld.IsKeyUp(Keys.OemPeriod))
+                    {
+                        level = (level + 1) % levelNames.Length;
+                        ChangeLevel(level);
+                    }
+                    if (keyboard.IsKeyDown(Keys.OemComma) && keyboardOld.IsKeyUp(Keys.OemComma))
+                    {
+                        level = (level - 1) % levelNames.Length;
+                        ChangeLevel(level);
+                    }
+
                     UpdatePlayer();
                     UpdateHook();
                     UpdateEnemies();
